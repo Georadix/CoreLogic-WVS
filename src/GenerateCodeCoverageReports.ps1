6a7e51bd638ee71e204b8d36bd22e66c46c4a09f
Stephane Alie <stephane@georadix.com>
2017-03-22 13:58:06 -0400

    Use script relative paths.

--- src/GenerateCodeCoverageReports.ps1
+++ src/GenerateCodeCoverageReports.ps1
@@ -1,36 +1,45 @@
 $cwd = Get-Location
 
-if (Test-Path env:APPVEYOR_BUILD_FOLDER) {
-    Set-Location $env:APPVEYOR_BUILD_FOLDER\src
-}
+$root = Split-Path -Parent $PSScriptRoot
+$src = $PSScriptRoot
 
-$opencover = (Resolve-Path "packages/OpenCover.*/tools/OpenCover.Console.exe" -ErrorAction Stop).ToString()
-$xunit = (Resolve-Path "packages/xunit.runner.console.*/tools/xunit.console.exe" -ErrorAction Stop).ToString()
-$reportgenerator = (Resolve-Path "packages/ReportGenerator.*/tools/ReportGenerator.exe" -ErrorAction Stop).ToString()
+#if (Test-Path env:APPVEYOR_BUILD_FOLDER) {
+#    Set-Location $env:APPVEYOR_BUILD_FOLDER\src
+#}
+
+$opencover = (Resolve-Path "$src/packages/OpenCover.*/tools/OpenCover.Console.exe" -ErrorAction Stop).ToString()
+$xunit = (Resolve-Path "$src/packages/xunit.runner.console.*/tools/xunit.console.exe" -ErrorAction Stop).ToString()
+$reportgenerator = (Resolve-Path "$src/packages/ReportGenerator.*/tools/ReportGenerator.exe" -ErrorAction Stop).ToString()
 $configuration = "Debug"
 
 if (Test-Path env:CONFIGURATION) {
     $configuration = $env:CONFIGURATION
 }
 
-cd ..
-
-if (!(Test-Path .\CodeCoverage)) {
-	mkdir CodeCoverage
+if (!(Test-Path "$root/CodeCoverage")) {
+	mkdir "$root/CodeCoverage"
 }
 
 & $opencover `
- -target:$xunit "-targetargs:"".\src\CoreLogic.Services.Wvs.Tests\bin\$configuration\CoreLogic.Services.Wvs.Tests.dll"" -nologo -noshadow" `
+ -target:$xunit "-targetargs:""$src\CoreLogic.Services.Wvs.Tests\bin\$configuration\CoreLogic.Services.Wvs.Tests.dll"" -nologo -noshadow" `
  -filter:"+[*]* -[*.Tests]* -[xunit*]*" `
  -excludebyattribute:"System.Diagnostics.DebuggerHiddenAttribute;System.Diagnostics.DebuggerNonUserCodeAttribute;System.Runtime.CompilerServices.CompilerGeneratedAttribute;System.CodeDom.Compiler.GeneratedCodeAttribute;System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute" `
  -coverbytest:* `
  -skipautoprops `
  -register:user `
- -output:.\CodeCoverage\OpenCover.xml
+ -output:"$root\CodeCoverage\OpenCover.xml"
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Error generating code coverage."
+}
 
 & $reportgenerator `
- -reports:.\CodeCoverage\OpenCover.xml `
- -targetdir:.\CodeCoverage\ReportGenerator `
+ -reports:"$root\CodeCoverage\OpenCover.xml" `
+ -targetdir:"$root\CodeCoverage\ReportGenerator" `
  -reporttypes:"Html;MHtml;badges"
 
-Set-Location $cwd
+if ($LASTEXITCODE -ne 0) {
+    throw "Error generating code coverage reports."
+}
+
+#Set-Location $cwd
