05094a24e8b7bc61d2141ce6c39934ea550f8367
Stephane Alie <stephane@georadix.com>
2017-03-22 16:18:41 -0400

    Clean code coverage folder + open report.

--- src/GenerateCodeCoverageReports.ps1
+++ src/GenerateCodeCoverageReports.ps1
@@ -1,20 +1,22 @@
 $root = Split-Path -Parent $PSScriptRoot
 $src = $PSScriptRoot
 
-$opencover = (Resolve-Path "$src/packages/OpenCover.*/tools/OpenCover.Console.exe" -ErrorAction Stop).ToString()
-$xunit = (Resolve-Path "$src/packages/xunit.runner.console.*/tools/xunit.console.exe" -ErrorAction Stop).ToString()
-$reportgenerator = (Resolve-Path "$src/packages/ReportGenerator.*/tools/ReportGenerator.exe" -ErrorAction Stop).ToString()
+$opencover = (Resolve-Path "$src\packages\OpenCover.*\tools\OpenCover.Console.exe" -ErrorAction Stop).ToString()
+$xunit = (Resolve-Path "$src\packages\xunit.runner.console.*\tools\xunit.console.exe" -ErrorAction Stop).ToString()
+$reportgenerator = (Resolve-Path "$src\packages\ReportGenerator.*\tools\ReportGenerator.exe" -ErrorAction Stop).ToString()
 $configuration = "Debug"
 
 if (Test-Path env:CONFIGURATION) {
     $configuration = $env:CONFIGURATION
 }
 
-if (!(Test-Path "$root/CodeCoverage")) {
-	mkdir "$root/CodeCoverage"
+if (!(Test-Path "$root\CodeCoverage")) {
+	mkdir "$root\CodeCoverage"
 }
 
-$testDlls = Get-ChildItem -Path .\src -Filter "*.Tests" | % { """""$src\$_\bin\$configuration\$_.dll""""" }
+Remove-Item "$root\CodeCoverage\*" -Recurse
+
+$testDlls = Get-ChildItem -Path $src -Filter "*.Tests" | % { """""$src\$_\bin\$configuration\$_.dll""""" }
 
 & $opencover `
  -target:$xunit "-targetargs:$testDlls -nologo -noshadow" `
@@ -36,4 +38,8 @@ if ($LASTEXITCODE -ne 0) {
 
 if ($LASTEXITCODE -ne 0) {
     throw "Error generating code coverage reports."
+}
+
+if (!(Test-Path env:APPVEYOR_BUILD_FOLDER)) {
+    Start-Process -FilePath "$root\CodeCoverage\ReportGenerator\index.htm"
 }
